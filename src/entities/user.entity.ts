f882ea0e43159f314b11181a4b9bd2dcad21b0f3
huangbo <huangbo@lj-ch.com>
2023-06-17 18:48:27 +0800

    表关联关系

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,6 +1,14 @@
-import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BeforeInsert,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
+import { Comment } from './comment.entity';
 import * as bcrypt from 'bcrypt';
+
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
@@ -21,6 +29,9 @@ export class User {
     nullable: false,
   })
   password: string;
+
+  @OneToMany((type) => Comment, (comment) => comment.user)
+  comments: Comment[];
 
   @BeforeInsert()
   async hashPassword() {
